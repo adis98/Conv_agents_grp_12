8073e0c58b9af82e72b72fdb0ae102d25b0d6c83
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2021-09-23 17:06:54 -0500

    Partially working, using Aditya's branch

--- SpaceReceptionist/SpaceReceptionist/src/main/kotlin/furhatos/app/spacereceptionist/flow/interaction.kt
+++ SpaceReceptionist/SpaceReceptionist/src/main/kotlin/furhatos/app/spacereceptionist/flow/interaction.kt
@@ -3,7 +3,27 @@ package furhatos.app.spacereceptionist.flow
 import furhatos.nlu.common.*
 import furhatos.flow.kotlin.*
 import furhatos.app.spacereceptionist.nlu.*
+import furhatos.nlu.common.Number
+import kotlin.math.floor
 import furhatos.flow.kotlin.onResponse as onResponse
+
+enum class RoomClass(val text: String) {
+    Suite("suite"), Citizen("citizen")
+}
+
+val availablePlaces = 40
+var requestNum : Int? = null
+var customerName : PersonName? = null
+var stayDuration : Duration? = null
+var roomType : RoomClass? = null
+var corrected_class: String? = null
+var roomCounts = mapOf(
+    RoomClass.Suite to 10,
+    RoomClass.Citizen to 20
+)
+var wishes: List<String> = listOf()
+//var suiteRoomCount = 10
+//var citizenRoomCount = 20
 
 val Start: State = state(Interaction) {
 
@@ -70,6 +90,204 @@ val HowManyGuests: State = state(Interaction) {
                 Let's get started then. How many people would you like to check-in?
                 """.trimIndent()
         )
+    }
+
+    onResponse<NumberOfPeople> {
+        requestNum = it.intent.numPeople
+        goto(RandomQuestion1)
+    }
+}
+
+val RandomQuestion1: State = state(Interaction) {
+    onEntry {
+        furhat.say(
+            """
+            Great. By the way, would you like to know about
+            the available amenities in our rooms?
+            """.trimIndent()
+        )
+    }
+
+    onResponse<Yes> {
+        furhat.say(
+            """
+            You are provided a bed, a table, a chair, and a Replicator,
+            which allows you to instantly create any dish you've ever
+            wanted to eat, in the comfort of your own room.
+            """.trimIndent()
+        )
+
+        goto(FurtherDetails)
+    }
+
+    onResponse<No> {
+        goto(FurtherDetails)
+    }
+}
+
+val FurtherDetails: State = state(Interaction) {
+    onEntry {
+        furhat.say(
+            """
+            Perfect. Now, could you give me your name, how long you intend
+            to stay on Starship Enterprise, and whether you would like to stay
+            in our Suite-class rooms or the Citizen-class rooms? (suite class
+            rooms have 2 beds, citizen-class have 1 bed).
+            """.trimIndent()
+        )
+    }
+
+    onResponse<Details> {
+        if (it.intent.name == null || it.intent.days == null || it.intent.roomChoice == null) {
+            furhat.say(
+                """
+            Sorry, I didn't understand one or more of your answers, can you repeat them?
+            """.trimIndent()
+            )
+        } else {
+            customerName = it.intent.name
+            stayDuration = it.intent.days
+
+            roomType = if (it.intent.roomChoice!!.equals("suite")) RoomClass.Suite else RoomClass.Citizen
+            val roomsNeeded = if (roomType!! == RoomClass.Citizen) requestNum else (floor(requestNum!! / 2f))
+
+            if (roomCounts[roomType!!]!! < requestNum!!) {
+                goto(StarshipOverloaded)
+            } else {
+                goto(SpecificWishes)
+            }
+        }
+    }
+}
+
+val FurtherDetails1: State = state(Interaction) {
+    onEntry {
+
+    }
+}
+
+val SpecificWishes: State = state(Interaction) {
+    onEntry {
+        furhat.say(
+            """
+            Amazing. The data has been entered to your name, $customerName.
+            Now, before asking you about the different activities we offer
+            on board, I would like to ask you if you have any specific wishes
+            for your stay here.
+            """.trimIndent()
+        )
+    }
+
+    onResponse<Yes>() {
+        furhat.say("Ok, please tell me your wish.")
+        goto(WishesLoop)
+    }
+
+    onResponse<No> {
+        goto(ThereAreNoWishes)
+    }
+}
+
+val WishesLoop: State = state(Interaction) {
+    onEntry {
+        furhat.say("Understood. Anything else?") // add variations
+    }
+}
+
+val ThereAreNoWishes: State = state(Interaction) {
+    onEntry {
+        furhat.say("Alright, then let's move on.")
+        goto(StarshipActivities)
+    }
+}
+
+val StarshipOverloaded: State = state(Interaction) {
+    onEntry {
+        furhat.say(
+            """
+            Unfortunately there are no rooms left of this kind. We only
+            have ${roomCounts[roomType]} of this kind free. Would you like to change
+            the number of people you are checking in?
+            """.trimIndent()
+        )
+    }
+
+    onResponse<Yes> {
+        goto(NumberOfPeopleChange)
+    }
+
+    onResponse<No> {
+        goto(CheckInCancel)
+    }
+
+    onTime(10000) {
+        goto(CheckInCancel)
+    }
+}
+
+val CheckInCancel: State = state(Interaction) {
+    onEntry {
+        furhat.say(
+            """
+            Alright then, please tell me if you'd like to
+            start over. Otherwise, I wish you a good day
+            """.trimMargin())
+    }
+
+    onTime(5000) {
+        goto(Idle)
+    }
+}
+
+val NumberOfPeopleChange: State = state(Interaction) {
+    onEntry {
+        furhat.say("Wonderful. Please tell me how many guests you would like me to check in.")
+    }
+
+    onResponse<Number>() {
+        requestNum = it.intent.value
+
+        if (roomCounts[roomType!!]!! < requestNum!!) {
+            goto(StarshipOverloaded)
+        } else {
+            goto(SpecificWishes)
+        }
+    }
+}
+
+val StarshipActivities: State = state(Interaction) {
+
+}
+
+val UserAsksForTooManyPlaces: State = state(Interaction) {
+    onEntry {
+        furhat.say(
+            """
+            Unfortunately we only have $availablePlaces places available.
+            """.trimIndent()
+        )
+
+        furhat.ask(
+            """
+            Would you like to change the number of guests, or cancel the
+            check-in?
+            """.trimIndent()
+        )
+    }
+
+    onResponse<Cancel> {
+        furhat.say("Ok, have a nice day.")
+        goto(Idle)
+    }
+
+    onResponse<ChangeNumGuests> {
+        furhat.say(
+            """
+            No problem.
+            """.trimIndent()
+        )
+
+        goto(HowManyGuests)
     }
 }
 
--- SpaceReceptionist/SpaceReceptionist/src/main/kotlin/furhatos/app/spacereceptionist/nlu/nlu.kt
+++ SpaceReceptionist/SpaceReceptionist/src/main/kotlin/furhatos/app/spacereceptionist/nlu/nlu.kt
@@ -1,6 +1,10 @@
 package furhatos.app.spacereceptionist.nlu
 
+import furhatos.nlu.ComplexEnumEntity
 import furhatos.nlu.Intent
+import furhatos.nlu.EnumEntity
+import furhatos.nlu.common.Number
+import furhatos.nlu.common.PersonName
 import furhatos.util.Language
 
 
@@ -18,6 +22,58 @@ class CheckIn : Intent() {
 class Confused : Intent() {
     override fun getExamples(lang: Language): List<String> {
         return listOf("Who are you?", "Where am I?", "What?", "What is this?", "What the hell is this?")
+    }
+}
+
+class Cancel : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("I'd like to cancel", "Cancel please", "Cancel")
+    }
+}
+
+class ChangeNumGuests : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+            "I'd like to modify the number of guests",
+            "I want to change the number of guests",
+            "Change",
+            "Change number of guests"
+        )
+    }
+}
+
+class NumberOfPeople : Intent() {
+    var numPeople: Int? = null
+
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+            "@numPeople people",
+            "@numPeople"
+        )
+    }
+}
+
+class RoomTypeEntity: EnumEntity() {
+    override fun getEnum(lang: Language): List<String> {
+        return listOf("suite", "citizen")
+    }
+}
+
+class Details : Intent() {
+    var name: PersonName? = null
+    var days: Duration? = null
+    val roomChoice: RoomTypeEntity? = null
+
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+            "@name @days @roomChoice"
+        )
+    }
+}
+
+class Duration(val days : Number? = Number(2)) : ComplexEnumEntity() {
+    override fun getEnum(lang: Language) : List<String> {
+        return listOf("@days days", "for @days days")
     }
 }
 
